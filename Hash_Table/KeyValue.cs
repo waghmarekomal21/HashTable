cd0b0ad0fc7b4ca2a938fe25de3982071d4ff629
unknown <Komal>
2022-09-23 21:04:35 +0530

    [Komal] Key Value

---
+++ Hash_Table/KeyValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+}
